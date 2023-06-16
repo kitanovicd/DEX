62a49bc47ff16ee54a2be4f71f578aa57cf9b202
kitanovicd <kitanovic.danilo@outlook.com>
2023-06-16 20:04:04 +0200

    Basic setup

---
+++ src/LiquidityPool.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract LiquidityPool {
+    address public token1;
+    address public token2;
+
+    function addLiquidity() external {}
+}
