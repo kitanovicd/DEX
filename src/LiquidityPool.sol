fc3824c1ce3ba46b6033669ecafbe35e1a465c67
kitanovicd <kitanovic.danilo@outlook.com>
2023-06-17 18:43:31 +0200

    Added swap limit and tests

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -5,11 +5,13 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import {console} from "forge-std/console.sol";
 
 contract LiquidityPool is ERC20 {
     using SafeERC20 for IERC20;
 
     uint256 public feePercentage;
+    uint256 public maxSwapPercentage;
 
     IERC20 public token1;
     IERC20 public token2;
@@ -30,11 +32,15 @@ contract LiquidityPool is ERC20 {
     constructor(
         IERC20 _token1,
         IERC20 _token2,
+        uint256 _feePercentage,
+        uint256 _maxSwapPercentage,
         string memory name,
         string memory symbol
     ) ERC20(name, symbol) {
         token1 = _token1;
         token2 = _token2;
+        feePercentage = _feePercentage;
+        maxSwapPercentage = _maxSwapPercentage;
     }
 
     function addLiquidity(
@@ -60,8 +66,6 @@ contract LiquidityPool is ERC20 {
             if (amount2 > amountIn2) {
                 amount2 = amountIn2;
                 amount1 = (amount2 * token1Balance) / token2Balance;
-
-                require(amount1 <= amountIn1, "Invalid amounts");
             }
 
             mintAmount = (amount1 * totalSupply()) / token1Balance;
@@ -97,5 +101,78 @@ contract LiquidityPool is ERC20 {
         token2.safeTransfer(msg.sender, amount2);
 
         emit RemoveLiquidity(msg.sender, amount1, amount2, burnAmount);
+    }
+
+    function swapExactInput(
+        IERC20 fromToken,
+        IERC20 toToken,
+        uint256 amountIn,
+        uint256 minAmountOut
+    ) external {
+        uint256 token1PoolBalance = token1.balanceOf(address(this));
+        uint256 token2PoolBalance = token2.balanceOf(address(this));
+
+        require(
+            amountIn <=
+                ((fromToken == token1 ? token1PoolBalance : token2PoolBalance) *
+                    maxSwapPercentage) /
+                    100,
+            "Exceeds max swap percentage"
+        );
+
+        uint256 amountInAfterFee = (amountIn * (100 - feePercentage)) / 100;
+        uint256 amountOut;
+
+        if (fromToken == token1) {
+            amountOut =
+                (amountInAfterFee * token2PoolBalance) /
+                token1PoolBalance;
+        } else {
+            amountOut =
+                (amountInAfterFee * token1PoolBalance) /
+                token2PoolBalance;
+        }
+
+        require(amountOut >= minAmountOut, "Insufficient amountOut");
+
+        fromToken.safeTransferFrom(msg.sender, address(this), amountIn);
+        toToken.safeTransfer(msg.sender, amountOut);
+    }
+
+    function swapExactOutput(
+        IERC20 fromToken,
+        IERC20 toToken,
+        uint256 maxAmountIn,
+        uint256 amountOut
+    ) external {
+        uint256 token1PoolBalance = token1.balanceOf(address(this));
+        uint256 token2PoolBalance = token2.balanceOf(address(this));
+
+        require(
+            amountOut <=
+                ((toToken == token1 ? token1PoolBalance : token2PoolBalance) *
+                    maxSwapPercentage) /
+                    100,
+            "Exceeds max swap percentage"
+        );
+
+        uint256 amountIn;
+
+        if (fromToken == token1) {
+            amountIn =
+                (amountOut * token1.balanceOf(address(this))) /
+                token2.balanceOf(address(this));
+        } else {
+            amountIn =
+                (amountOut * token2.balanceOf(address(this))) /
+                token1.balanceOf(address(this));
+        }
+
+        amountIn = (amountIn * (100 + feePercentage)) / 100;
+
+        require(amountIn <= maxAmountIn, "Insufficient amountIn");
+
+        fromToken.safeTransferFrom(msg.sender, address(this), amountIn);
+        toToken.safeTransfer(msg.sender, amountOut);
     }
 }
--- test/LiquidityPool.t.sol
+++ test/LiquidityPool.t.sol
@@ -10,6 +10,8 @@ import {console} from "forge-std/Console.sol";
 contract LiquidityPoolTest is Test {
     string public constant SYMBOL = "LP";
     string public constant NAME = "LiquidityPoolToken";
+    uint256 public constant FEE_PERCENTAGE = 5;
+    uint256 public constant MAX_SWAP_PERCENTAGE = 10;
 
     address public alice;
     address public bob;
@@ -33,7 +35,14 @@ contract LiquidityPoolTest is Test {
         token1.mint(bob, 100000000000000000 ether);
         token2.mint(bob, 100000000000000000 ether);
 
-        liquidityPool = new LiquidityPool(token1, token2, NAME, SYMBOL);
+        liquidityPool = new LiquidityPool(
+            token1,
+            token2,
+            FEE_PERCENTAGE,
+            MAX_SWAP_PERCENTAGE,
+            NAME,
+            SYMBOL
+        );
     }
 
     function testSetup() public {
@@ -261,6 +270,234 @@ contract LiquidityPoolTest is Test {
             burnAmount,
             0,
             expectedToReceiveToken2 + 1
+        );
+
+        vm.stopPrank();
+    }
+
+    function testSwapExactInput(
+        uint256 amount1,
+        uint256 amount2,
+        uint256 amountToSwap
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 1 ether, 100 ether);
+        amountToSwap = bound(amountToSwap, 1 ether, amount1 / 10);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 token1BobBalanceBefore = token1.balanceOf(bob);
+        uint256 token2BobBalanceBefore = token2.balanceOf(bob);
+        uint256 feeAmount = (amountToSwap * FEE_PERCENTAGE) / 100;
+        uint256 shouldReceiveToken2Amount = (amount2 *
+            (amountToSwap - feeAmount)) / amount1;
+
+        token1.approve(address(liquidityPool), amountToSwap);
+        liquidityPool.swapExactInput(token1, token2, amountToSwap, 0);
+
+        vm.stopPrank();
+
+        uint256 token1BobBalanceAfter = token1.balanceOf(bob);
+        uint256 token2BobBalanceAfter = token2.balanceOf(bob);
+
+        assertEq(token1BobBalanceAfter, token1BobBalanceBefore - amountToSwap);
+        assertTrue(
+            token2BobBalanceAfter >=
+                token2BobBalanceBefore + shouldReceiveToken2Amount - 10 &&
+                token2BobBalanceAfter <=
+                token2BobBalanceBefore + shouldReceiveToken2Amount + 10
+        );
+        assertEq(
+            token1.balanceOf(address(liquidityPool)),
+            amount1 + amountToSwap
+        );
+        assertTrue(
+            token2.balanceOf(address(liquidityPool)) <=
+                amount2 - shouldReceiveToken2Amount + 10 &&
+                token2.balanceOf(address(liquidityPool)) >=
+                amount2 - shouldReceiveToken2Amount - 10
+        );
+    }
+
+    function testSwapExactInputRevertInsufficientAmountOut(
+        uint256 amount1,
+        uint256 amount2,
+        uint256 amountToSwap
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 1 ether, 100 ether);
+        amountToSwap = bound(amountToSwap, 1 ether, amount1 / 10);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 shouldReceiveToken2Amount = (amount2 *
+            ((amountToSwap * (100 - FEE_PERCENTAGE)) / 100)) / amount1;
+
+        token1.approve(address(liquidityPool), amountToSwap);
+
+        vm.expectRevert(bytes("Insufficient amountOut"));
+        liquidityPool.swapExactInput(
+            token1,
+            token2,
+            amountToSwap,
+            shouldReceiveToken2Amount + 1
+        );
+
+        vm.stopPrank();
+    }
+
+    function testSwapExactInputRevertExceedsMaxSwapPercentage(
+        uint256 amount1,
+        uint256 amount2,
+        uint256 amountToSwap
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 1 ether, 100 ether);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 amountToSwap = ((amount1 * MAX_SWAP_PERCENTAGE) / 100) + 1;
+        token1.approve(address(liquidityPool), amountToSwap);
+
+        vm.expectRevert(bytes("Exceeds max swap percentage"));
+        liquidityPool.swapExactInput(token1, token2, amountToSwap, 0);
+
+        vm.stopPrank();
+    }
+
+    function testSwapExactOutput(
+        uint256 amount1,
+        uint256 amount2,
+        uint256 amountToReceive
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 10 ether, 100 ether);
+        amountToReceive = bound(amountToReceive, 1 ether, amount2 / 10);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 token1BobBalanceBefore = token1.balanceOf(bob);
+        uint256 token2BobBalanceBefore = token2.balanceOf(bob);
+        uint256 maxAmountIn = (((amount1 * (amountToReceive)) / amount2) *
+            (100 + FEE_PERCENTAGE)) / 100;
+
+        token1.approve(address(liquidityPool), maxAmountIn);
+        liquidityPool.swapExactOutput(
+            token1,
+            token2,
+            maxAmountIn,
+            amountToReceive
+        );
+
+        vm.stopPrank();
+
+        uint256 token1BobBalanceAfter = token1.balanceOf(bob);
+        uint256 token2BobBalanceAfter = token2.balanceOf(bob);
+
+        assertEq(token1BobBalanceAfter, token1BobBalanceBefore - maxAmountIn);
+        assertEq(
+            token2BobBalanceAfter,
+            token2BobBalanceBefore + amountToReceive
+        );
+        assertEq(
+            token1.balanceOf(address(liquidityPool)),
+            amount1 + maxAmountIn
+        );
+        assertEq(
+            token2.balanceOf(address(liquidityPool)),
+            amount2 - amountToReceive
+        );
+    }
+
+    function testSwapExactOutputRevertInsufficientAmountIn(
+        uint256 amount1,
+        uint256 amount2,
+        uint256 amountToReceive
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 10 ether, 100 ether);
+        amountToReceive = bound(amountToReceive, 1 ether, amount2 / 10);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 maxAmountIn = (((amount1 * (amountToReceive)) / amount2) *
+            (100 + FEE_PERCENTAGE)) / 100;
+
+        token1.approve(address(liquidityPool), maxAmountIn - 1);
+
+        vm.expectRevert(bytes("Insufficient amountIn"));
+        liquidityPool.swapExactOutput(
+            token1,
+            token2,
+            maxAmountIn - 1,
+            amountToReceive
+        );
+
+        vm.stopPrank();
+    }
+
+    function testSwapExactOutputRevertExceedsMaxSwapPercentage(
+        uint256 amount1,
+        uint256 amount2
+    ) public {
+        amount1 = bound(amount1, 10 ether, 100 ether);
+        amount2 = bound(amount2, 10 ether, 100 ether);
+
+        vm.startPrank(alice);
+
+        token1.approve(address(liquidityPool), amount1);
+        token2.approve(address(liquidityPool), amount2);
+        liquidityPool.addLiquidity(amount1, amount2, 0);
+
+        vm.stopPrank();
+        vm.startPrank(bob);
+
+        uint256 maxAmountIn = type(uint256).max;
+        uint256 amountToReceive = ((amount2 * MAX_SWAP_PERCENTAGE) / 100) + 1;
+
+        token1.approve(address(liquidityPool), maxAmountIn);
+
+        vm.expectRevert(bytes("Exceeds max swap percentage"));
+        liquidityPool.swapExactOutput(
+            token1,
+            token2,
+            maxAmountIn,
+            amountToReceive
         );
 
         vm.stopPrank();
