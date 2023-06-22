739204b043ba7a55d13615b999318f84de0eb4c1
kitanovicd <kitanovic.danilo@outlook.com>
2023-06-22 17:52:33 +0200

    Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,31 +2,18 @@
 pragma solidity >=0.8.0;
 
 import {Script} from "forge-std/Script.sol";
-import {LiquidityPool} from "../src/LiquidityPool.sol";
+import {DEX} from "../src/DEX.sol";
 
 contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
-        /*  vm.startBroadcast();
+        vm.startBroadcast();
 
-        token1 = new MintableERC20("Token1", "T1");
-        token2 = new MintableERC20("Token2", "T2");
+        new MintableERC20("Token1", "T1");
+        new MintableERC20("Token2", "T2");
+        new DEX();
 
-        token1.mint(0x0, 100000000000000000 ether);
-        token2.mint(0x0, 100000000000000000 ether);
-        token1.mint(0x0, 100000000000000000 ether);
-        token2.mint(0x0, 100000000000000000 ether);
-
-        liquidityPool = new LiquidityPool(
-            token1,
-            token2,
-            5,
-            10,
-            "Liquidity Pool",
-            "LP"
-        );
-
-        vm.stopBroadcast();*/
+        vm.stopBroadcast();
     }
 }
